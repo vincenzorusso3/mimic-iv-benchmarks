84502f32059382a9cecca19bdb137480cf34d103
Vincenzo Russo <vincenzorusso12@users.noreply.github.com>
2021-04-26 15:47:18 +0200

    add files

---
+++ mimic4benchmark/generaCSVTestSet.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import numpy
+
+folder = os.listdir('./') #
+#del folder[-5:]
+
+myProb=[]
+for i in range(len(folder)):
+    test = numpy.random.choice(numpy.arange(0, 2), p=[0.8, 0.2])
+    myProb.append(test)
+
+print(len(myProb))
+print(len(folder))
+
+df = pd.DataFrame(myProb,folder)
+#print(df[0].value_counts())
+df.to_csv('testset.csv',header=False)
